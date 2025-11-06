891de40c21354da45175d2451ee511392eaeb6cd
mohammed <mg3235566@gmail.com>
2025-11-06 13:50:01 +0200

    change the name of button after updated

--- js/test.js
+++ js/test.js
@@ -65,7 +65,7 @@ function updateElement(index) {
   pc.value = product.Category.trim();
   pd.value = product.Description.trim();
   currentIndex = index;
-  document.getElementById("addProduct").textContent = "updated";
+  document.getElementById("addProduct").textContent = "upDated";
 }
 //CRUD operations
 //Create
