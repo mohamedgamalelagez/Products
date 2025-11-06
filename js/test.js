3d4652ecfa7c52d7e4f05a740f3fa4a91417cf39
mohammed <mg3235566@gmail.com>
2025-11-06 15:41:58 +0200

    change the name of button

--- js/test.js
+++ js/test.js
@@ -22,7 +22,7 @@ function addProduct() {
   } else {
     allProducts[currentIndex] = product;
     currentIndex = null;
-    document.getElementById("addProduct").textContent = "addProduct";
+    document.getElementById("addProduct").textContent = "ADDPRODUCT";
   }
   clearForm();
   displayAllProduct();
@@ -65,7 +65,7 @@ function updateElement(index) {
   pc.value = product.Category.trim();
   pd.value = product.Description.trim();
   currentIndex = index;
-  document.getElementById("addProduct").textContent = "upDated";
+  document.getElementById("addProduct").textContent = "UPDATE";
 }
 //CRUD operations
 //Create
